00d0141ca47f86c463330869c6447e20a4c13e65
arash <A.Shahidi@campus.lmu.de>
2026-03-27 02:32:06 +0100

    study-overview

--- .projio/projio.mk
+++ .projio/projio.mk
@@ -2,20 +2,21 @@
 # Include from your Makefile: -include .projio/projio.mk
 
 PYTHON  ?= python
-DATALAD ?= datalad
-PROJIO  ?= projio
+DATALAD ?= /storage/share/python/environments/Anaconda3/envs/labpy/bin/datalad
+MKDOCS  ?= /storage/share/python/environments/Anaconda3/envs/labpy/bin/python -m mkdocs
+PROJIO  ?= /storage/share/python/environments/Anaconda3/envs/rag/bin/python -m projio
 MSG     ?= Update
 
 .PHONY: save push url
 .PHONY: projio-init projio-config-user projio-config-show projio-status projio-auth
-.PHONY: projio-gh projio-gl projio-ria site-build site-serve site-stop site-list site-detect mcp
+.PHONY: projio-gh projio-gl projio-ria site-build site-serve site-stop site-list site-detect mcp mcp-config
 
 # --- DataLad targets ---
 save:
 	$(DATALAD) save -m "$(MSG)"
 
 push:
-	$(DATALAD) push --to github
+	$(DATALAD) push --to gitlab
 
 url:
 	$(PROJIO) url -C .
@@ -62,3 +63,6 @@ site-detect:
 
 mcp:
 	$(PROJIO) mcp -C .
+
+mcp-config:
+	$(PROJIO) mcp-config -C . --yes
